e1ff0b3641a53c0798c58dc428c53341c5cbda34
Asiye Eldes <eldesasiye@gmail.com>
2020-11-14 23:12:10 +0200

    first commit

--- src/test/java/gmibank/pages/LoginPage.java
+++ src/test/java/gmibank/pages/LoginPage.java
@@ -19,6 +19,30 @@ public class LoginPage {
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement signInButton;
 
+    @FindBy(xpath = "//span[.='Cancel']")
+    public WebElement cancelButton;
+
+    @FindBy(id="account-menu")
+    public WebElement loginDropDown;
+
+    @FindBy(id="login-item")
+    public WebElement signInText;
+
+    @FindBy(xpath = "//div[@role='dialog']")
+    public String signInPage;
+
+    @FindBy(xpath = "(//div[@class='alert alert-warning fade show'])[1]")
+    public WebElement resetPasswordOption;
+
+    @FindBy(xpath="//*[.='Reset your password']")
+    public WebElement resetPasswordNewText;
+
+    @FindBy(xpath = "(//div[@class='alert alert-warning fade show'])[2]//a//span")
+    public WebElement againRegisterOption;
+
+    @FindBy(id = "register-title")
+    public WebElement registerTitle;
+
 
 
 
