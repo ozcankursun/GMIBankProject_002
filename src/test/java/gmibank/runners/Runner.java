9d6f2420e5dea8c10af999e82052169107150a89
trhnays3 <69141077+trhnays3@users.noreply.github.com>
2020-11-19 00:38:39 +0300

    Update Runner.java

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -10,11 +10,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-<<<<<<< HEAD
-        tags = "@address",
-=======
         tags = "@US01",
->>>>>>> master
         dryRun = false
 )
 
