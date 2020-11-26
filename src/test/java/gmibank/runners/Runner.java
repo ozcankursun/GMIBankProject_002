b31e8f47dc65f77843ad36e43d4a57d403586e32
Mahmut <mhmtygt.3090@gmail.com>
2020-11-26 13:21:43 +0200

    master

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
         tags = "@CreateNewCountry",
-        dryRun = true
+        dryRun = false
 )
 class Runner {
 }
